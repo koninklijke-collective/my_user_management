6243bbfdde3f3b759a4d807a7e9d0641f4e3edc7
Benjamin Serfhos <benjamin@serfhos.com>
2017-08-29 10:39:30 +0200

    [TASK] Update version pointer

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,24 +4,31 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'My Backend User Management',
     'description' => 'A module that makes it possible for editors to maintain backend users.',
     'category' => 'module',
-    'version' => '3.2.0',
+    'version' => '3.3.4',
     'state' => 'stable',
-    'uploadFolder' => false,
-    'clearCacheOnLoad' => true,
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearcacheonload' => true,
     'author' => 'Benjamin Serfhos',
     'author_email' => 'benjamin@serfhos.com',
     'author_company' => 'Rotterdam School of Management, Erasmus University',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'internal' => '',
+    'modify_tables' => '',
+    'lockType' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.99.99',
-            'beuser' => '7.6.0-8.99.99',
+            'typo3' => '7.6.0-8.7.99',
+            'beuser' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [
-            'KoninklijkeCollective\\MyUserManagement\\' => 'Classes'
-        ]
+            'KoninklijkeCollective\\MyUserManagement\\' => 'Classes',
+        ],
     ],
 ];
