8705423093aa7ce86d03e4cc7877a09cf54c598f
Benjamin Serfhos <benjamin@serfhos.com>
2021-02-02 14:29:32 +0100

    [TASK] Add mapping for extbase repository

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \KoninklijkeCollective\MyUserManagement\Domain\Model\BackendUser::class => [
+        'tableName' => 'be_users',
+        'properties' => [
+            'allowedLanguages' => [
+                'fieldName' => 'allowed_languages',
+            ],
+            'fileMountPoints' => [
+                'fieldName' => 'file_mountpoints',
+            ],
+            'dbMountPoints' => [
+                'fieldName' => 'db_mountpoints',
+            ],
+            'backendUserGroups' => [
+                'fieldName' => 'usergroup',
+            ],
+            'createdBy' => [
+                'fieldName' => 'cruser_id',
+            ],
+        ],
+    ],
+    \KoninklijkeCollective\MyUserManagement\Domain\Model\BackendUserGroup::class => [
+        'tableName' => 'be_groups',
+        'properties' => [
+            'subGroups' => [
+                'fieldName' => 'subgroup',
+            ],
+            'databaseMountPoints' => [
+                'fieldName' => 'db_mountpoints',
+            ],
+            'fileMountPoints' => [
+                'fieldName' => 'file_mountpoints',
+            ],
+        ],
+    ],
+    \KoninklijkeCollective\MyUserManagement\Domain\Model\FileMount::class => [
+        'tableName' => 'sys_filemounts',
+        'properties' => [
+            'title' => [
+                'fieldName' => 'title'
+            ],
+            'path' => [
+                'fieldName' => 'path'
+            ],
+            'isAbsolutePath' => [
+                'fieldName' => 'base'
+            ],
+            'storage' => [
+                'fieldName' => 'base'
+            ],
+        ],
+    ]
+];
